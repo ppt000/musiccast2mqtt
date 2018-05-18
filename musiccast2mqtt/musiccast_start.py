59c9d65f36276bd3cc1a86776cadbbf5184b468d
ppt000 <paolo.taddonio@empiluma.com>
2018-05-18 13:38:29 +0100

    change name of launcher module
    From musiccast2mqtt (which is the same name of the package)
    To musiccast_start.

--- musiccast2mqtt/musiccast_start.py
+++ musiccast2mqtt/musiccast_start.py
@@ -2,16 +2,18 @@
 
 import os.path
 
-import mqtt_gateways.utils.app_properties as app
+import mqttgateway.utils.app_properties as app
 app.Properties.init(os.path.realpath(__file__))
 
 # import the module that initiates and starts the gateway
-import mqtt_gateways.gateway.start_gateway as start_g
-#import mqtt_gateways.gateway.start_gateway_test as start_g # TEST!!!!!
+import mqttgateway.gateway.start_gateway as start_g
 
 # import the module representing the interface *** add your import here ***
-import mqtt_gateways.musiccast.musiccast_interface as mci
+import musiccast2mqtt.musiccast_interface as mci
 
-if __name__ == '__main__':
+def main():
     # launch the gateway *** add your class here ***
     start_g.startgateway(mci.musiccastInterface)
+
+if __name__ == '__main__':
+    main()
