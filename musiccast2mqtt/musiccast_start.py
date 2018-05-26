0c895a306a6f5877fde59b16ed51e4c0838a498f
ppt000 <paolo.taddonio@empiluma.com>
2018-05-26 06:38:53 +0100

    Revert "update app.Properties call"
    This reverts commit b55e6f7a192b2e4de4361a9fe20203ef11134ef6.

--- musiccast2mqtt/musiccast_start.py
+++ musiccast2mqtt/musiccast_start.py
@@ -2,10 +2,10 @@
 
 import os.path
 
-_APP_NAME = 'musiccast2mqtt'
+_PROJECT_NAME = 'musiccast2mqtt'
 
 import mqttgateway.utils.app_properties as app
-app.Properties.init(app_path=os.path.realpath(__file__), app_name=_APP_NAME)
+app.Properties.init(os.path.realpath(__file__), app_name=_PROJECT_NAME)
 
 # import the module that initiates and starts the gateway
 import mqttgateway.gateway.start_gateway as start_g
