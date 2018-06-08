da5f35938a3717ef137deef16284978c13d753fe
ppt000 <paolo.taddonio@empiluma.com>
2018-06-08 16:22:19 +0100

    cleanup of unused methods + docstrings and comments

--- musiccast2mqtt/musiccast_input.py
+++ musiccast2mqtt/musiccast_input.py
@@ -1,7 +1,6 @@
-'''
-Created on 5 May 2018
+''' Representation of Inputs.
 
-@author: PierPaolo
+.. reviewed 31 May 2018
 '''
 
 import musiccast2mqtt.musiccast_exceptions as mcx
@@ -16,6 +15,7 @@ class Input(object):
         data (dictionary): represents the input
         device (:class:`Device`): the parent device
     '''
+
     def __init__(self, data, device):
         self.device = device
         self.id = data['id']
@@ -23,47 +23,56 @@ class Input(object):
         return
 
     def post_init(self):
-        ''' Docstring'''
+        ''' Post initialisation.
+
+        To be overridden.
+        '''
         pass
 
-    def get_source_zone(self, raises=False):
+    def get_control_zone(self, raises=False):
         ''' Returns the control zone connected to this input'''
-        return self # by default
+        return self.device.zones[0] # TODO: should we define a 'priority' zone here?
 
-    def is_source(self):
-        ''' Tests if this is a source.'''
-        if not isinstance(self, Source):
-            raise mcx.ConfigError(''.join(('This input <', self.id, '> should be a source.')))
-        return
+    #===============================================================================================
+    # def is_source(self):
+    #     ''' Tests if this is a source.'''
+    #     if not isinstance(self, Source):
+    #         raise mcx.ConfigError(''.join(('This input <', self.id, '> should be a source.')))
+    #     return
+    #===============================================================================================
 
-    def get_source(self):
-        ''' Returns the source playing through this input.'''
-        # CHECK: difference with get_source_zone?
-        return self # by default
+    #===============================================================================================
+    # def get_source(self):
+    #     ''' Returns the source playing through this input.'''
+    #     return self # by default
+    #===============================================================================================
 
 class Feed(Input):
     ''' Represents an input on the device that is not a source.
 
-    TODO: A feed within a MusicCast system is an input for which the `play_info_type`
+    A feed within a MusicCast system is an input for which the `play_info_type`
     field within the getFeatures structure is set to **none**.
 
     Args:
         feed_data (dictionary): the **feed** characteristics.
         device (:class:`Device`): the parent device.
     '''
+
     def __init__(self, feed_data, device):
         super(Feed, self).__init__(feed_data, device)
+        # the following attributes are specific to a feed
         self._remote_dev_id = feed_data['device_id']
         self._remote_zone_id = feed_data.get('zone_id', None)
         self._remote_dev = None # assigned later when all devices are initialised
         self._remote_zone = None # assigned later and only for MusicCast devices
-        #self.remote_source = None
         return
 
     def post_init(self):
-        ''' Docstring '''
-        super(Feed, self).post_init()
+        ''' Post initialisation.
 
+        Finds the actual objects from the system description that have the assigned ids.
+        '''
+        super(Feed, self).post_init()
         self._remote_dev = self.device.system.get_device(device_id=self._remote_dev_id)
         if self._remote_dev is None: # device for that feed was not found
             _logger.info(''.join(('Device ', self._remote_dev_id, ' not defined.')))
@@ -75,40 +84,41 @@ class Feed(Input):
                                           ' not found in device ', self._remote_dev_id, '.')))
                     self._remote_dev = None # disable the device pointer if zone not found
 
-    def get_remote_dev(self, raises=False):
-        ''' Returns the remote Device object.'''
-        if self._remote_dev is None and raises:
-            raise mcx.ConfigError('Current [remote device] unassigned')
-        return self._remote_dev
+    def get_control_zone(self, raises=False):
+        ''' Returns the remote Zone object.
 
-    def get_source_zone(self, raises=False):
-        ''' Returns the remote Zone object.'''
+        Args:
+            raises (boolean): if True, raises an exception if not found
+        '''
         if self._remote_zone is None and raises:
-            raise mcx.ConfigError('Current [remote zone] unassigned')
+            raise mcx.ConfigError('Remote zone unassigned')
         return self._remote_zone
 
-    def get_remote_source(self, raises=False):
-        ''' Finds the remote Source object playing through this feed.'''
-        if self._remote_zone is None:
-            if raises: raise mcx.ConfigError('Current [remote zone] unassigned.')
-            else: return None
-        return self._remote_zone.get_current_input(raises=True)
+    def get_remote_dev(self, raises=False):
+        ''' Returns the remote Device object.
+
+        Args:
+            raises (boolean): if True, raises an exception if not found
+        '''
+        if self._remote_dev is None and raises:
+            raise mcx.ConfigError('Remote device unassigned')
+        return self._remote_dev
 
 class Source(Input):
     ''' Represents a source on the device.
 
     A source within a MusicCast system is an input for which the
-    `play_info_type` field within the getFeatures structure is set to a
-    different value than **none**, normally either **cd**, **tuner** or
-    **netusb**.
+    ``play_info_type`` field within the getFeatures structure is set to a
+    different value than ``none``, normally either ``cd``, ``tuner`` or
+    ``netusb``.
 
     Args:
         source_data (dictionary): the **source** characteristics.
         device (:class:`Device`): the parent device.
     '''
+
     def __init__(self, source_data, device):
         super(Source, self).__init__(source_data, device)
-        #self.usedby = [] # TODO: Remove?
         self.playinfo_type = None # set in load_musiccast
         return
 
@@ -116,7 +126,7 @@ class Source(Input):
         '''Initialisation of MusicCast related characteristics.
 
         This method uses the objects retrieved from previous HTTP requests.
-        It can be called again at any time to try again this initialisation.
+        It can be called at any time to try again this initialisation.
         '''
         try:
             for inp in self.device.get_feature(('system', 'input_list')):
